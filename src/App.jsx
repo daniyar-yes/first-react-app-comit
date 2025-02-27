3a94be62097b7d88e765fd8008a0d62594ff18a7
EvgeniyaKalinichenko <gusewa.evgenia2011@yandex.ru>
2025-02-26 20:35:20 -0600

    first step

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import { useEffect } from "react";
+
 function App() {
+
+  useEffect(() => 
+    async function fetchApi() {
+      try {
+        const response = await fetch("https://randomuser.me/api/")
+      }
+      catch (error) {
+        console.log(error);
+    }
+    // console.log("Hello");
+    fetchApi();
+}, []);
+
   return <>Hello</>;
 }
+
 
 export default App;
