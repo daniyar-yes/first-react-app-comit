3bc1ca4e3f620f5550214e3c504469131abece08
Sara Costa Cabral Mululo <saramululo@gmail.com>
2025-02-26 20:31:07 -0600

    added fetchApi function

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
+import { useEffect } from 'react';
+
 function App() {
+
+useEffect (() => {
+  async function fetchApi(){
+    try{
+      const response = await fetch('https://randomuser.me/api/');
+      
+    }
+    catch (error){
+      console.log(error);
+    }
+  }
+  console.log('API calls go here');
+  
+  fetchApi()
+}, []);
+
   return <>Hello</>;
 }
+
+
+
 
 export default App;
