473a1327b54edaac7660334514929e20319e3b78
FatimaMubeenSh <shfatimamubeen@gmail.com>
2025-03-04 08:48:37 +0500

    User Profile with Pic Updates

--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -1,24 +1,39 @@
 /* eslint-disable react/prop-types */
+import styles from './UserProfile.module.css';
+
 const UserProfile = ({ data }) => {
+  const default_img = 'https://media.istockphoto.com/id/1409329028/vector/no-picture-available-placeholder-thumbnail-icon-illustration-design.jpg?s=612x612&w=0&k=20&c=_zOuJu755g2eEUioiOUdz_mHKJQJn-tDgIAhQzyeKUQ='
+
   const dataToShow = data
     ? data
     : {
         name: {
-          first: "FN NA",
-          last: "LN NA",
-          title: "Title NA",
+          first: "",
+          last: "",
+          title: "",
         },
-        id: "ID NA",
-        age: "AGE NA",
-      };
-  console.log(dataToShow);
+        id: "",
+        age: "",
+      imgSrc: {
+        large: default_img,
+        medium: default_img,
+        thumbnail: default_img,
+        },
+    };
+  
+  // console.log(dataToShow);
+  const fullName = data ? `${dataToShow.name.title}. ${dataToShow.name.first} ${dataToShow.name.last}` : "";
+  const age = data ? `Age: ${dataToShow.age}` : "";
 
   return (
     <>
-      <p>
-        {`Name: ${dataToShow.name.title} ${dataToShow.name.first} ${dataToShow.name.last}`}
-      </p>
-      <p>{`Age: ${dataToShow.age}`}</p>
+      <div className={styles.container}>
+        <img src={dataToShow.imgSrc.large} alt="User Image" className={styles.userImage} />
+        <div className={styles.textContainer}>
+          <p>{fullName}</p>
+          <p>{age}</p>
+        </div>
+      </div>
     </>
   );
 };
