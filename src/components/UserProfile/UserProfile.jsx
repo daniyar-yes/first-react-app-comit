15ec45a614edb221b042aba5b6ce1ac757ea29ad
daniyar-yes <yeskaliyevdan@hotmail.com>
2025-03-03 20:11:31 -0800

    loading spinner

--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -1,8 +1,9 @@
 /* eslint-disable react/prop-types */
-import styles from './UserProfile.module.css';
+import styles from "./UserProfile.module.css";
 
 const UserProfile = ({ data }) => {
-  const default_img = 'https://media.istockphoto.com/id/1409329028/vector/no-picture-available-placeholder-thumbnail-icon-illustration-design.jpg?s=612x612&w=0&k=20&c=_zOuJu755g2eEUioiOUdz_mHKJQJn-tDgIAhQzyeKUQ='
+  const default_img =
+    "https://media.istockphoto.com/id/1409329028/vector/no-picture-available-placeholder-thumbnail-icon-illustration-design.jpg?s=612x612&w=0&k=20&c=_zOuJu755g2eEUioiOUdz_mHKJQJn-tDgIAhQzyeKUQ=";
 
   const dataToShow = data
     ? data
@@ -14,24 +15,35 @@ const UserProfile = ({ data }) => {
         },
         id: "",
         age: "",
-      imgSrc: {
-        large: default_img,
-        medium: default_img,
-        thumbnail: default_img,
+        imgSrc: {
+          large: default_img,
+          medium: default_img,
+          thumbnail: default_img,
         },
-    };
-  
+      };
+
   // console.log(dataToShow);
-  const fullName = data ? `${dataToShow.name.title}. ${dataToShow.name.first} ${dataToShow.name.last}` : "";
-  const age = data ? `Age: ${dataToShow.age}` : "";
+  const fullName = `${dataToShow.name.title}. ${dataToShow.name.first} ${dataToShow.name.last}`;
+  const age = `Age: ${dataToShow.age}`;
 
   return (
     <>
       <div className={styles.container}>
-        <img src={dataToShow.imgSrc.large} alt="User Image" className={styles.userImage} />
+        <img
+          src={dataToShow.imgSrc.large}
+          alt="User Image"
+          className={styles.userImage}
+        />
         <div className={styles.textContainer}>
-          <p>{fullName}</p>
-          <p>{age}</p>
+          {data ? (
+            <>
+              <p>{fullName}</p>
+              <p>{age}</p>
+            </>
+          ) : (
+            <div className={styles.loadingSpinner}></div>
+            
+          )}
         </div>
       </div>
     </>
