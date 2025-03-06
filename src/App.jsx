22dda898088b51177f5fc6659223914f789dc3d7
daniyar-yes <yeskaliyevdan@hotmail.com>
2025-03-05 18:47:42 -0800

    handle isLoading in UserProfile

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,57 @@
-import { useEffect, useState } from 'react';
-import UserProfile from './components/UserProfile/UserProfile';
+import { useEffect, useState } from "react";
+import UserProfile from "./components/UserProfile/UserProfile";
 
 function App() {
-
   const [userData, setUserData] = useState(null);
-  // const displayName = userData ? userData?.results[0]?.name?.first : 'LOADING';
-  
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
+    setIsLoading(true);
     async function fetchApi() {
       try {
-        const response = await fetch('https://randomuser.me/api/');
+        const response = await fetch("https://randomuser.me/api/");
         const data = await response.json();
         console.log(data);
         const userProfileData = {
           name: {
             first: data.results[0].name.first,
             last: data.results[0].name.last,
-            title: data.results[0].name.title
+            title: data.results[0].name.title,
           },
           id: data.results[0].login.uuid,
           age: data.results[0].dob.age,
           imgSrc: {
             large: data.results[0].picture.large,
-            medium: data.results[0].picture.medium , 
-            thumbnail: data.results[0].picture.thumbnail ,
-          }
-        }
+            medium: data.results[0].picture.medium,
+            thumbnail: data.results[0].picture.thumbnail,
+          },
+        };
         setUserData(userProfileData);
-      }
-      catch (error) {
-        console.log(error);
+      } catch (error) {
+        console.error(error)
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
       }
     }
 
-    fetchApi()
+    fetchApi();
   }, []);
 
- 
+  const userProfileData = {}
 
+  const datesDiagramData = {}
+
+  const mapsData = {}
 
   return (
     <>
-   
-    {/* <p>{displayName}</p> */}
-    <UserProfile data={userData} />
+      <UserProfile data={userData} isLoading={isLoading} error={error} />
+      <MapsComponent coordinates={mapsData} />
+      <TimeLine dates={datesDiagramData} />
     </>
   );
 }
-
-
-
 
 export default App;
--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -1,49 +1,30 @@
 /* eslint-disable react/prop-types */
 import styles from "./UserProfile.module.css";
 
-const UserProfile = ({ data }) => {
-  const default_img =
-    "https://media.istockphoto.com/id/1409329028/vector/no-picture-available-placeholder-thumbnail-icon-illustration-design.jpg?s=612x612&w=0&k=20&c=_zOuJu755g2eEUioiOUdz_mHKJQJn-tDgIAhQzyeKUQ=";
+const UserProfile = ({ data, isLoading, error }) => {
 
-  const dataToShow = data
-    ? data
-    : {
-        name: {
-          first: "",
-          last: "",
-          title: "",
-        },
-        id: "",
-        age: "",
-        imgSrc: {
-          large: default_img,
-          medium: default_img,
-          thumbnail: default_img,
-        },
-      };
+    if (isLoading) {
+       return <div className={styles.loadingSpinner}></div>
+    }
 
-  // console.log(dataToShow);
-  const fullName = `${dataToShow.name.title}. ${dataToShow.name.first} ${dataToShow.name.last}`;
-  const age = `Age: ${dataToShow.age}`;
+    if (error) {
+        return <p>Sorry, error message says: {error}</p>
+    }
+
+  const fullName = `${data?.name?.title}. ${data?.name?.first} ${data?.name?.last}`;
+  const age = `Age: ${data?.age}`;
 
   return (
     <>
       <div className={styles.container}>
         <img
-          src={dataToShow.imgSrc.large}
-          alt="User Image"
+          src={data?.imgSrc.large}
+          alt={`Profile picture of ${fullName}`}
           className={styles.userImage}
         />
         <div className={styles.textContainer}>
-          {data ? (
-            <>
-              <p>{fullName}</p>
-              <p>{age}</p>
-            </>
-          ) : (
-            <div className={styles.loadingSpinner}></div>
-            
-          )}
+          <p>{fullName}</p>
+          <p>{age}</p>
         </div>
       </div>
     </>
