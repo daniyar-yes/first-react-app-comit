5cc7f8ed24b9f42210bdd1ae1c4df3d3d7943546
TaosifShaad <taosifshaad000@gmail.com>
2025-03-05 22:04:39 -0600

    Add timeLine component

--- src/components/TimeLine/index.jsx
+++ src/components/TimeLine/index.jsx
@@ -2,19 +2,27 @@
 import styles from "./TimeLine.module.css";
 
 const TimeLine = ({ data, isLoading, error }) => {
+  if (isLoading || data === null) {
+    return <div className={styles.loadingSpinner}></div>;
+  }
 
-    if (isLoading || data === null) {
-       return <div className={styles.loadingSpinner}></div>
-    }
+  if (error) {
+    return <p>Sorry, error message says: {error}</p>;
+  }
 
-    if (error) {
-        return <p>Sorry, error message says: {error}</p>
-    }
-
+  console.log(data.dob.slice(0, 10));
+  const dob = data.dob.slice(0, 10);
+  const registered = data.registered.slice(0, 10);
+  const currentDate = new Date();
+  const formattedCurrentDate = currentDate.toISOString().split("T")[0];
 
   return (
     <>
-      hi Time
+      <div className={styles.container}>
+        <span>{dob}</span>
+        <span>{registered}</span>
+        <span>{formattedCurrentDate}</span>
+      </div>
     </>
   );
 };
