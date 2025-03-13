314c65c8087e7889ac7fc59e4cc214233802c079
daniyar-yes <yeskaliyevdan@hotmail.com>
2025-03-12 20:30:46 -0700

    linked canvas via ref and draw rectangle

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,52 @@
 import "./App.css";
+import { useEffect, useRef } from "react";
 
 function App() {
+  const canvasRef = useRef(null);
+  const canvasContextRef = useRef(null);
+  useEffect(() => {
+    if (canvasRef.current) {
+      canvasContextRef.current = canvasRef.current.getContext("2d");
+      let ctx = canvasContextRef.current;
+      ctx.beginPath();
+      ctx.arc(95, 50, 80, 0, 2 * Math.PI);
+      ctx.stroke();
+    }
+  }, []);
+
+  const handleClick = () => {
+    if (canvasRef.current) {
+      canvasContextRef.current = canvasRef.current.getContext("2d");
+      let ctx = canvasContextRef.current;
+      ctx.beginPath();
+      ctx.arc(95, 50, 800, 0, 2 * Math.PI);
+      ctx.stroke();
+      ctx.fillStyle = "red";
+      ctx.fillRect(10, 150, 200, 75);
+  }
+}
+
+const handleClear = () => {
+  if (canvasRef.current) {
+    canvasContextRef.current = canvasRef.current.getContext("2d");
+    let ctx = canvasContextRef.current;
+  ctx.clearRect(0,0, 100,150);
+}
+}
 
   return (
     <>
-      <canvas width="500px" height="300px" style={{border: '3px solid red'}}></canvas>
-
+    <div>Overlay</div>
+      <canvas
+        ref={canvasRef}
+        width="500px"
+        height="300px"
+        style={{ border: "3px solid red" }}
+      ></canvas>
+      <button onClick={handleClick}>Draw Rectangle</button>
+      <button onClick={handleClear}>Clear Rect Area</button>
     </>
   );
 }
 
-export default App
+export default App;
